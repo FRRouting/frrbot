1023b6eeb836c3b5c6ab8cf40942c6912ca75a02
Quentin Young <qlyoung@qlyoung.net>
2019-12-05 19:30:27 +0000

    change run.sh to launch bot w/o unix domain sock
    Signed-off-by: Quentin Young <qlyoung@qlyoung.net>

--- run.sh
+++ run.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-
-uwsgi -s /tmp/frrbot.sock --manage-script-name --enable-threads --mount /frrbot=frrbot:app
-sudo chmod o+w /tmp/frrbot.sock
+uwsgi -s 127.0.0.1:3031 --manage-script-name --enable-threads --mount /frrbot=frrbot:app
